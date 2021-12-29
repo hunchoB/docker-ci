d6639a4ebe58a39b46159da4e3ff67e4eb34cadb
root <root@localhost.localdomain>
2021-12-29 16:52:34 +0300

    Docker

--- src/app/settings.py
+++ src/app/settings.py
@@ -7,7 +7,8 @@ from split_settings.tools import include
 from app.conf.environ import env
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+
+SECRET_KEY = r'xMUQH?=~O:s&ms}F-fT:aF+fioi}::"Mc*MK&UN;Swke[h\`'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', cast=bool, default=False)
